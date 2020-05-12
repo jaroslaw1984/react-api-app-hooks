0b92b3277b96d03fedf8ddda270a434c508eab8e
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-05-12 16:45:34 +0200

    component it a main handler to select a user

--- src/components/userCard/UserCard.js
+++ src/components/userCard/UserCard.js
@@ -1,13 +1,40 @@
 import React from "react";
-import Details from "./details/Details";
+import UserItem from "./userItem/UserItem";
 
-const UserCard = () => {
-  return (
-    <div className="card">
-      <p>card</p>
-      <Details />
-    </div>
-  );
-};
+class UserCard extends React.Component {
+  state = {
+    index: 0,
+  };
 
+  handleChangeIndexUp = () => {
+    this.setState({
+      index: this.state.index + 1,
+    });
+  };
+
+  handleChangeIndexDown = () => {
+    this.setState({
+      index: this.state.index - 1,
+    });
+  };
+
+  render() {
+    const { users } = this.props;
+    const { index } = this.state;
+
+    return (
+      <div className="card">
+        <img src={users[index].picture.large} alt={users[index].name.first} />
+        <p>
+          {users[index].name.first} {users[index].name.last}
+        </p>
+        {/* {users.map((user) => (
+        <UserItem key={user.login.salt} user={user} />
+      ))} */}
+        <button onClick={this.handleChangeIndexUp}>Next</button>
+        <button onClick={this.handleChangeIndexDown}>Previus</button>
+      </div>
+    );
+  }
+}
 export default UserCard;
