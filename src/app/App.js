4272c61c938f8b4a79e31382e3c299fb88597b14
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-05-12 16:39:20 +0200

    State now store users that are fetched form http

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,50 @@
 import React, { Component } from "react";
 import UserCard from "../components/userCard/UserCard";
 import SearchCard from "../components/searchCard/SearchCard";
+import Footer from "../components/footer/Footer";
 
 class App extends Component {
+  state = {
+    users: [],
+    isloading: false,
+    showCard: false,
+    genderMale: true,
+  };
+
+  handleShowCard = () => {
+    this.setState({
+      showCard: true,
+    });
+  };
+
+  async componentDidMount() {
+    try {
+      await fetch(
+        `https://randomuser.me/api/?results=5&gender=${
+          this.state.genderMale ? "female" : "male"
+        }`
+      )
+        .then((respond) => respond.json())
+        .then((data) => {
+          const user = data.results;
+
+          this.setState((prevState) => ({
+            users: [...prevState.users, ...user],
+          }));
+        });
+    } catch (error) {
+      throw error(error);
+    }
+  }
+
   render() {
+    const { users, isloading, genderMale, showCard } = this.state;
+    console.log(users);
     return (
-      <div className="App">
-        <UserCard />
-        <SearchCard />
+      <div className="container">
+        {showCard && <UserCard users={users} />}
+        <SearchCard click={this.handleShowCard} />
+        <Footer />
       </div>
     );
   }
