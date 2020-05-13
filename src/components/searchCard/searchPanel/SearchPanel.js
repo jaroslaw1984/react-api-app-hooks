d5f5067a6fc23aaaadb4f4eea5e1345bb857cd7c
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-05-13 16:39:32 +0200

    Code refactoring

--- src/components/searchCard/searchPanel/SearchPanel.js
+++ src/components/searchCard/searchPanel/SearchPanel.js
@@ -1,15 +1,13 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-const SearchPanel = ({ getUsers, male, female, checked }) => {
-  return (
-    <div>
-      <label htmlFor="male">Male</label>
-      <input type="radio" id="male" checked={checked} onChange={male} />
-      <label htmlFor="female">Female</label>
-      <input type="radio" id="female" checked={!checked} onChange={female} />
-      <button onClick={getUsers}>Search</button>
-    </div>
-  );
-};
+const SearchPanel = ({ getUsers, male, female, checked }) => (
+  <Fragment>
+    <label htmlFor="male">Male</label>
+    <input type="radio" id="male" checked={checked} onChange={male} />
+    <label htmlFor="female">Female</label>
+    <input type="radio" id="female" checked={!checked} onChange={female} />
+    <button onClick={getUsers}>Search</button>
+  </Fragment>
+);
 
 export default SearchPanel;
