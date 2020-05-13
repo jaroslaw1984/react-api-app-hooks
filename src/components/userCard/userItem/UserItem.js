21fcd3e2c23ada494c6ccd2889473a461cedd256
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-05-13 16:56:11 +0200

    Created component that contains information about a single users

--- src/components/userCard/userItem/UserItem.js
+++ src/components/userCard/userItem/UserItem.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-const UserItem = ({ user }) => {
-  return (
-    <div>
-      <img src={user.picture.large} alt={user.name.first} />
-      <p>
-        {user.name.first} {user.name.last}
-      </p>
-    </div>
-  );
-};
+const UserItem = ({ users, index, next, previus }) => (
+  <Fragment>
+    <img src={users[index].picture.large} alt={users[index].name.first} />
+    <p>
+      {users[index].name.first} {users[index].name.last}
+    </p>
+    {/* condition when button will hide and show */}
+    {users[index] !== users[0] && <button onClick={previus}>Previus</button>}
+    {users[index] !== users[4] && <button onClick={next}>Next</button>}
+  </Fragment>
+);
 
 export default UserItem;
