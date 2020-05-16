98bb8ce77e740345b140d7b80991d6832dae647c
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-05-16 14:08:45 +0200

    Displayed message that were added to the object

--- src/components/userCard/userItem/UserItem.js
+++ src/components/userCard/userItem/UserItem.js
@@ -1,18 +1,22 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 
-const UserItem = ({ users, index, next, previus }) => (
-  <Fragment>
-    <img src={users[index].picture.large} alt={users[index].name.first} />
-    <p>
-      {users[index].name.first} {users[index].name.last}
-    </p>
-    {/* condition when button will hide and show */}
-    {users[index] !== users[0] && <button onClick={previus}>Previus</button>}
-    {/* Remember to change last index depends how many api will get users from api  */}
-    {users[index] !== users[4] && <button onClick={next}>Next</button>}
-  </Fragment>
-);
+const UserItem = ({ users, index, next, previus }) => {
+  return (
+    <Fragment>
+      <img src={users[index].picture.large} alt={users[index].name.first} />
+      <h4>Hey, my name is</h4>
+      <h2>
+        {users[index].name.first} {users[index].name.last}
+      </h2>
+      <h4>{users[index].msg.text}</h4>
+      {/* condition when button will hide and show if index is below first showed person.*/}
+      {users[index] !== users[0] && <button onClick={previus}>Previus</button>}
+      {/* Remember to change last index depends how many api will get users from api  */}
+      {users[index] !== users[4] && <button onClick={next}>Next</button>}
+    </Fragment>
+  );
+};
 
 UserItem.propTypes = {
   users: PropTypes.array.isRequired,
