14ac249e6a8be24d48af472a3d47f1fe2bc6e216
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-05-18 10:45:09 +0200

    Convert class component to function component and implemented hooks to UserCard

--- src/components/userCard/UserCard.js
+++ src/components/userCard/UserCard.js
@@ -1,42 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import UserItem from "./userItem/UserItem";
 import PropTypes from "prop-types";
 
-class UserCard extends React.Component {
-  state = {
-    index: 0,
-  };
-
-  // check if users array is array
-  static propTypes = {
-    users: PropTypes.array.isRequired,
-  };
+const UserCard = ({ users }) => {
+  const [index, setIndex] = useState(0);
 
   // it change and increase index of users array
-  handleChangeIndexNext = () => {
-    this.setState({
-      index: this.state.index + 1,
-    });
+  const handleChangeIndexNext = () => {
+    setIndex(index + 1);
   };
 
   // it change and decreases index of users array
-  handleChangeIndexPrevius = () => {
-    this.setState({
-      index: this.state.index - 1,
-    });
+  const handleChangeIndexPrevius = () => {
+    setIndex(index - 1);
   };
 
-  render() {
-    return (
-      <div className="card">
-        <UserItem
-          users={this.props.users}
-          index={this.state.index}
-          next={this.handleChangeIndexNext}
-          previus={this.handleChangeIndexPrevius}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="card">
+      <UserItem
+        users={users}
+        index={index}
+        next={handleChangeIndexNext}
+        previus={handleChangeIndexPrevius}
+      />
+    </div>
+  );
+};
+
+// check if users array is array
+UserCard.propTypes = {
+  users: PropTypes.array.isRequired,
+};
+
 export default UserCard;
