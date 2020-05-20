e0351aa2c851042c85659258482f441fbfa584dd
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-05-20 11:00:17 +0200

    Create Reducer and Actions

---
+++ src/components/context/resources/AppReducer.js
@@ -0,0 +1,53 @@
+import {
+  SEARCH_USERS,
+  SET_USERS,
+  SET_LOADING,
+  SET_GENDER,
+  SET_CHECKED,
+  SET_INCREMENT,
+  SET_DECREMENT,
+} from "../types";
+
+export default (state, action) => {
+  switch (action.type) {
+    case SEARCH_USERS:
+      return {
+        ...state,
+        users: action.payload,
+        isLoading: false,
+        index: 0,
+      };
+    case SET_USERS:
+      return {
+        ...state,
+        users: [],
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case SET_CHECKED:
+      return {
+        ...state,
+        isChecked: action.to,
+      };
+    case SET_INCREMENT:
+      return {
+        ...state,
+        index: state.index + action.by,
+      };
+    case SET_DECREMENT:
+      return {
+        ...state,
+        index: state.index - action.by,
+      };
+    case SET_GENDER:
+      return {
+        ...state,
+        genderMale: action.to,
+      };
+    default:
+      return state;
+  }
+};
