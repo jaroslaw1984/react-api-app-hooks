81072f4dd2b150a8b9da74bb48ffe2810f3d7207
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-05-20 10:52:14 +0200

    Created home component

---
+++ src/components/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import UserCard from "../../userCard/UserCard";
+import SearchCard from "../../searchCard/SearchCard";
+
+const Home = () => (
+  <Fragment>
+    <UserCard />
+    <SearchCard />
+  </Fragment>
+);
+
+export default Home;
