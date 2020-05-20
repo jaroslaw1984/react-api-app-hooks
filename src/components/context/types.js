3938a26b0b06dd3b2df4e48713c29e6f0585e551
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-05-20 11:02:36 +0200

    Initial variables of types

--- src/components/context/types.js
+++ src/components/context/types.js
@@ -1,5 +1,7 @@
 export const SEARCH_USERS = "SEARCH_USERS";
+export const SET_USERS = "SET_USERS";
 export const SET_LOADING = "SET_LOADING";
 export const SET_GENDER = "SET_GENDER";
 export const SET_CHECKED = "SET_CHECKED";
-export const SET_INDEX = "SET_INDEX";
+export const SET_INCREMENT = "SET_INCREMENT";
+export const SET_DECREMENT = "SET_DECREMENT";
