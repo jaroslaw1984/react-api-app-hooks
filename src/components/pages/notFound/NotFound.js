1d2d0cc2fea37a681dfddb784f07eae89c692854
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-05-21 08:59:50 +0200

    Component implementation, page not found

--- src/components/pages/notFound/NotFound.js
+++ src/components/pages/notFound/NotFound.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
     <div>
       <h1>Not Found 404</h1>
       <p>The page you are looking for does not exits</p>
+      <Link to={"/"}>Home page</Link>
     </div>
   );
 };
