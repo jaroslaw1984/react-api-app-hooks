a8e14f4c666f866f1a861ff67c2550cd9656bca6
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-06-03 12:11:53 +0200

    Adding appearance and operation of the modal

--- src/components/favorite/Modal/Modal.js
+++ src/components/favorite/Modal/Modal.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import AppContext from "../../context/resources/appContext";
 
 const Modal = () => {
+  const appContext = useContext(AppContext);
+
+  const { handleCloseModal, favoriteUsers, users, index } = appContext;
+
+  const name = users[index].name.first;
+  const last = users[index].name.last;
+
   return (
     <div className="modal">
-      <span>Modal</span>
+      <div className="modal__close" onClick={handleCloseModal}></div>
+      <div className="modal__containter">
+        {favoriteUsers.map((user) => (
+          <div className="modal__elem">
+            <img
+              src={user.picture.medium}
+              alt={user.name.first}
+              className="modal__img"
+            />
+            <h5>
+              {user.name.first} {user.name.last}
+            </h5>
+            <Link to={`/details/${name.toLowerCase()}${last.toLowerCase()}`}>
+              <button className="button button--state-more">More</button>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
