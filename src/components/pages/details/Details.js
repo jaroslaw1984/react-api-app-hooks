79ed4f75f6da21a99a816cfaa3f3ec5f85a97412
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-06-04 12:42:40 +0200

    Fixed an issue that caused the person added to favorites to be male and the next was female, which resulted in the loss of male data

--- src/components/pages/details/Details.js
+++ src/components/pages/details/Details.js
@@ -13,22 +13,30 @@ const Details = ({ match }) => {
     users,
     index,
     rating,
+    favoriteUsers,
     handleSetRate,
     handlePutToFavorite,
   } = appContext;
 
-  // find user with specific condition
+  // find user in users array with specific condition
   const user = users.find(
     (user) =>
       user.name.first.toLowerCase() + user.name.last.toLowerCase() ===
       match.params.name
   );
 
+  // find user in favorite array if user were not found in user array
+  const fav = favoriteUsers.find(
+    (fav) =>
+      fav.name.first.toLowerCase() + fav.name.last.toLowerCase() ===
+      match.params.name
+  );
+
   // condition if user will refresh the page at this endpoint
-  if (user === undefined) {
+  if (user === undefined && fav === undefined) {
     return (
       <Fragment>
-        <h1>User data no longer exists</h1>
+        <h1>Users data no longer exists</h1>
         <Link to="/">Go back</Link>
       </Fragment>
     );
@@ -36,16 +44,25 @@ const Details = ({ match }) => {
     return (
       <div className="details">
         <div className="details__person">
-          <img src={user.picture.large} alt={user.name.first} />
+          <img
+            src={user === undefined ? fav.picture.large : user.picture.large}
+            alt={user === undefined ? fav.name.first : user.name.first}
+          />
           <h1>
-            {user.name.first} {user.name.last}
+            {user === undefined ? fav.name.first : user.name.first}
+            {user === undefined ? fav.name.last : user.name.last}
           </h1>
         </div>
         <div className="details__info">
-          <h2>Age: {user.dob.age}</h2>
-          <p>Country: {user.location.country}</p>
-          <p>City: {user.location.city}</p>
-          <p>E-mail: {user.email}</p>
+          <h2>Age: {user === undefined ? fav.dob.age : user.dob.age}</h2>
+          <p>
+            Country:{" "}
+            {user === undefined ? fav.location.country : user.location.country}
+          </p>
+          <p>
+            City: {user === undefined ? fav.location.city : user.location.city}
+          </p>
+          <p>E-mail: {user === undefined ? fav.email : user.email}</p>
         </div>
 
         <Link to="/">Go back</Link>
