0c1a5fc3b786d90938b3b6d6aee90927ebdac211
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-06-05 13:41:58 +0200

    Added condition that allow read rated user from favorite array if user array is empty

--- src/components/userCard/rateUser/RateUser.js
+++ src/components/userCard/rateUser/RateUser.js
@@ -1,22 +1,34 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
+import AppContext from "../../context/resources/appContext";
 
-const RateUser = ({ users, index, setRating, rating }) => {
+const RateUser = () => {
+  const appContext = useContext(AppContext);
+
+  const { users, index, handleSetRate, rating, favoriteUsers } = appContext;
+
   useEffect(() => {
-    // debugger;
-    // save user rate to current user
-    if (users[index].rating === 0 && rating !== undefined)
-      users[index].rating = rating;
+    // save user rate to current user also if array users are empty look user rate in favoriteUsers array
+    if (
+      users[index] === undefined
+        ? favoriteUsers[index].rating === 0
+        : users[index].rating === 0 && rating !== undefined
+    )
+      users[index] === undefined
+        ? (favoriteUsers[index].rating = rating)
+        : (users[index].rating = rating);
     else {
       // this condition prevent from infnity loop
       if (rating > 0) return;
       else {
         // set value to app state
-        setRating(users[index].rating);
+        users[index] === undefined
+          ? handleSetRate(favoriteUsers[index].rating)
+          : handleSetRate(users[index].rating);
       }
     }
-  }, [index, setRating, users, rating]);
+  }, [index, handleSetRate, users, rating, favoriteUsers]);
 
   return (
     <Fragment>
@@ -29,7 +41,7 @@ const RateUser = ({ users, index, setRating, rating }) => {
                 type="radio"
                 name="rate"
                 value={value}
-                onClick={() => setRating(value)}
+                onClick={() => handleSetRate(value)}
               />
               <FontAwesomeIcon
                 icon={faStar}
