26a65ce25a3e09178ae3377a223f6f03a0049bf6
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-06-05 15:48:38 +0200

    Adding logic to component

---
+++ src/components/userCard/isUserExists/IsUserExists.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const IsUserExists = ({ click, name, last, isUserExists }) => {
+  return (
+    <div
+      className={
+        isUserExists
+          ? "attention attention--active"
+          : "attention attention--hide"
+      }
+    >
+      <h3>
+        {name} {last}
+      </h3>
+      <p>already exists in favorite bookmark</p>
+      <button onClick={click}>OK</button>
+    </div>
+  );
+};
+
+IsUserExists.propTypes = {
+  click: PropTypes.bool.isRequired,
+  isUserExists: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+  last: PropTypes.string.isRequired,
+};
+
+export default IsUserExists;
