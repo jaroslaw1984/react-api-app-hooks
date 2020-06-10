672cba864e59d926af8cc0b43a74722bfe99342b
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-06-10 13:23:11 +0200

    Adding condition of height and styles

--- src/components/userCard/UserCard.js
+++ src/components/userCard/UserCard.js
@@ -6,10 +6,14 @@ import Loading from "../loading/Loading";
 const UserCard = () => {
   const appContext = useContext(AppContext);
 
-  const { isLoading, users } = appContext;
+  const { isLoading, users, isHeight } = appContext;
 
   return (
-    <div className="card">
+    <div
+      className={isHeight ? "card--active" : "card"}
+      style={isHeight ? { height: "300px" } : { height: 0 }}
+    >
+      {/* <div className="card"> */}
       {isLoading ? <Loading /> : users.length > 0 && <UserItem />}
     </div>
   );
