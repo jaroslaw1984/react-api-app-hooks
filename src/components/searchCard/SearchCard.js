906f0658907bf5d2479da665a0e2b08b8f0ae3f0
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-06-10 12:03:15 +0200

    Adding <main> tag

--- src/components/searchCard/SearchCard.js
+++ src/components/searchCard/SearchCard.js
@@ -2,10 +2,12 @@ import React from "react";
 import SearchPanel from "./searchPanel/SearchPanel";
 
 const SearchCard = () => (
-  <div className="search__card">
-    <h2 className="search__card__header">Find your other half</h2>
-    <SearchPanel />
-  </div>
+  <main>
+    <div className="search__card">
+      <h2 className="search__card__header">Find your other half</h2>
+      <SearchPanel />
+    </div>
+  </main>
 );
 
 export default SearchCard;
