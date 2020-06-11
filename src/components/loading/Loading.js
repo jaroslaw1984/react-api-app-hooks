2e539441921b30aa772f4c588f83b1bff2a0035d
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-06-11 12:46:22 +0200

    Created containter for loading img and moved inline style to scss file

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,20 +1,10 @@
-import React, { Fragment } from "react";
+import React from "react";
 import loading from "./searchIcon.svg";
 
 const Loading = () => (
-  <Fragment>
-    <img
-      src={loading}
-      alt=""
-      className="spinner"
-      style={{
-        width: "150px",
-        height: "150px",
-        margin: "0 auto",
-        display: "block",
-      }}
-    />
-  </Fragment>
+  <div className="spinner">
+    <img src={loading} alt="" className="spinner__img" />
+  </div>
 );
 
 export default Loading;
