2e25f39ab6045dfe6b427723530a883e6d24d0d4
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-06-12 14:16:13 +0200

    Component Details were styled and added new class names

--- src/components/pages/details/Details.js
+++ src/components/pages/details/Details.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import AppContect from "../../context/resources/appContext";
 import RateUser from "../../userCard/rateUser/RateUser";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 
 const Details = ({ match }) => {
   const appContext = useContext(AppContect);
@@ -36,47 +36,70 @@ const Details = ({ match }) => {
   if (user === undefined && fav === undefined) {
     return (
       <Fragment>
-        <h1>Users data no longer exists</h1>
-        <Link to="/">Go back</Link>
+        <h1 className="not__exists__h1">This person no longer exists</h1>
+        <Link to="/">
+          <button className="button not__exists__button--back">Back</button>
+        </Link>
       </Fragment>
     );
   } else {
     return (
-      <div className="details">
-        <div className="details__person">
-          <img
-            src={user === undefined ? fav.picture.large : user.picture.large}
-            alt={user === undefined ? fav.name.first : user.name.first}
-          />
-          <h1>
-            {user === undefined ? fav.name.first : user.name.first}
-            {user === undefined ? fav.name.last : user.name.last}
-          </h1>
+      <div className="wrapper">
+        <div className="details">
+          <div className="details__info info">
+            <h2>Age: {user === undefined ? fav.dob.age : user.dob.age}</h2>
+            <p>
+              Country:{" "}
+              <span>
+                {user === undefined
+                  ? fav.location.country
+                  : user.location.country}
+              </span>
+            </p>
+            <p>
+              City:{" "}
+              <span>
+                {user === undefined ? fav.location.city : user.location.city}
+              </span>
+            </p>
+            <p>
+              E-mail: <span>{user === undefined ? fav.email : user.email}</span>
+            </p>
+            <Link to="/">
+              <button className="button button__more--back">Back</button>
+            </Link>
+          </div>
+          <div className="details__person">
+            <div className="details__person__img">
+              <img
+                src={
+                  user === undefined ? fav.picture.large : user.picture.large
+                }
+                alt={user === undefined ? fav.name.first : user.name.first}
+              />
+            </div>
+            <div className="details__person__name">
+              <h1>
+                {user === undefined ? fav.name.first : user.name.first}{" "}
+                {user === undefined ? fav.name.last : user.name.last}
+              </h1>
+            </div>
+          </div>
+          <div className="details__nav nav">
+            <FontAwesomeIcon
+              icon={faUserPlus}
+              className="plus"
+              onClick={() => handlePutToFavorite(users[index])}
+            />
+            <RateUser
+              users={users}
+              index={index}
+              setRating={handleSetRate}
+              className="stars"
+              rating={rating}
+            />
+          </div>
         </div>
-        <div className="details__info">
-          <h2>Age: {user === undefined ? fav.dob.age : user.dob.age}</h2>
-          <p>
-            Country:{" "}
-            {user === undefined ? fav.location.country : user.location.country}
-          </p>
-          <p>
-            City: {user === undefined ? fav.location.city : user.location.city}
-          </p>
-          <p>E-mail: {user === undefined ? fav.email : user.email}</p>
-        </div>
-
-        <Link to="/">Go back</Link>
-        <RateUser
-          users={users}
-          index={index}
-          setRating={handleSetRate}
-          rating={rating}
-        />
-        <FontAwesomeIcon
-          icon={faPlus}
-          className="plus"
-          onClick={() => handlePutToFavorite(users[index])}
-        />
       </div>
     );
   }
