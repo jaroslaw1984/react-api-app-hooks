743319d4750764118f23acab83ab3ad9fac0c9df
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-06-14 16:55:33 +0200

    Refactoring component and styles has been added

--- src/components/pages/notFound/NotFound.js
+++ src/components/pages/notFound/NotFound.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>Not Found 404</h1>
-      <p>The page you are looking for does not exits</p>
-      <Link to={"/"}>Home page</Link>
-    </div>
+    <Fragment>
+      <div className="container__not__found">
+        <h2>Not Found 404</h2>
+        <p>The page you are looking for does not exits</p>
+      </div>
+      <Link to={"/"}>
+        <button className="button button__not__found">Back</button>
+      </Link>
+    </Fragment>
   );
 };
 
